dba95f98594bea30dc68464c3976c196d5d633c6
Alejandro Rodríguez Salamanca <alexrs95@gmail.com>
2016-12-28 16:14:53 +0100

    insertion mutation and fitness function

--- code/insertion.m
+++ code/insertion.m
@@ -4,31 +4,29 @@
 %	2 : path representation
 %
 
-function NewChrom = insertion(OldChrom,Representation)
+function NewChrom = insertion(OldChrom)
 
-NewChrom=OldChrom;
-
-if Representation==1 
-	NewChrom=adj2path(NewChrom);
+    NewChrom = OldChrom;
+    % select two positions in the tour
+    rndi = zeros(1,2);
+    while rndi(1) == rndi(2)
+        rndi=rand_int(1,2,[1 size(NewChrom,2)]);
+    end
+    rndi = sort(rndi);
+    
+    % get the value of the first random position
+    temp = NewChrom(rndi(1));
+    % insert this value in the second random position
+    NewChrom = insertAt(NewChrom, temp, rndi(2));
+    % remove the first random position
+    NewChrom(rndi(1)) = [];
+    % End of function
 end
 
-% select two positions in the tour
-
-rndi=zeros(1,2);
-
-while rndi(1)==rndi(2)
-	rndi=rand_int(1,2,[1 size(NewChrom,2)]);
+function arrOut = insertAt(arr,val,index)
+    if index == numel(arr)+1
+        arrOut = [arr val];
+    else
+        arrOut = [arr(1:index-1) val arr(index:end)];
+    end
 end
-rndi = sort(rndi);
-
-NewChrom(rndi(1):rndi(2)) = NewChrom(rndi(2):-1:rndi(1));
-%buffer=NewChrom(rndi(1));
-%NewChrom(rndi(1))=NewChrom(rndi(2));
-%NewChrom(rndi(2))=buffer;
-
-
-if Representation==1
-	NewChrom=path2adj(NewChrom);
-end
-
-% End of function
--- code/tspfun.m
+++ code/tspfun.m
@@ -1,7 +1,7 @@
 %
 % ObjVal = tspfun(Phen, Dist)
 % Implementation of the TSP fitness function
-%	Phen contains the phenocode of the matrix coded in adjacency
+%	Phen contains the phenocode of the matrix coded in path
 %	representation
 %	Dist is the matrix with precalculated distances between each pair of cities
 %	ObjVal is a vector with the fitness values for each candidate tour 
@@ -9,9 +9,19 @@
 %
 
 function ObjVal = tspfun(Phen, Dist)
-	ObjVal=Dist(Phen(:,1),1);
-	for t=2:size(Phen,2)  
-    	ObjVal=ObjVal+Dist(Phen(:,t),t);
+    % the objective function works with adjacency representation. In this
+    % version, path representation is used, so the fitness function should
+    % be adapted. Now, the phenotype is converted to adjacency
+    % representation first, and then, the Objective Value is computed as it
+    % was computed in the original version.
+    adj = zeros(size(Phen));
+    for row=1:size(Phen)
+       adj(row,:) = path2adj(Phen(row,:));
+    end
+    
+    ObjVal = Dist(adj(:,1), 1);
+	for t=2:size(adj,2)  
+    	ObjVal=ObjVal + Dist(adj(:,t), t);
 	end
 
 % End of function
