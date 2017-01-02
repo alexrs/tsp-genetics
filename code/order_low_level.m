f941392d4b1655275d9e82016817d3b89b38f0ad
Alejandro Rodríguez Salamanca <alexrs95@gmail.com>
2017-01-02 16:09:49 +0100

    Fix crossover

--- code/order_low_level.m
+++ code/order_low_level.m
@@ -15,26 +15,38 @@ function Offspring=order_low_level(Parents)
     start_index = rand_int(1, 1, [1, cols - 1]);
     end_index = rand_int(1, 1, [start_index + 1, cols]);
 
-    Offspring(1, start_index:end_index) = Parents(1, start_index:end_index);
-    Offspring(2, start_index:end_index) = Parents(2, start_index:end_index);
+    Offspring(1, start_index:end_index) = Parents(2, start_index:end_index);
+    Offspring(2, start_index:end_index) = Parents(1, start_index:end_index);
 
 
     for off=1:2
-        % Create aux matrix to check the parent that has not been copied a segment
         Buff = Parents(off,:);
+        Buff = [Buff(end_index+1:end), Buff(1:end_index)];
 
         members = ismember(Buff, Offspring(off, :));
-
-        % Take only the values from aux different to zero (the values not copied in the offspring)
-
-        Buff = Buff(members == 0);
-
-        %Copy not used values in the offspring in the order they appear in the second parent (or first for the second round)
-
-        % From second crossover point to the end of the vector
+        Buff(members == 1) = 0;
         
-        Offspring(off, end_index+1:end) = Buff(start_index:end);
-        Offspring(off, 1:start_index - 1) = Buff(1:start_index - 1);
+        ii = 1;
+        X = find(Buff);
+        for jj=1:start_index - 1
+           if Buff(X(ii)) ~= 0
+              Offspring(off, jj) = Buff(X(ii));
+              Buff(X(ii)) = 0;
+              ii = mod(ii, cols) + 1;
+           end
+        end
+        
+        ii = 1;
+        X = find(Buff);
+        for jj=end_index + 1:cols
+           if Buff(X(ii)) ~= 0
+              Offspring(off, jj) = Buff(X(ii));
+              Buff(X(ii)) = 0;
+              ii = mod(ii, cols) + 1;
+           end
+        end
+        %Offspring(off, end_index+1:end) = Buff(start_index:end);
+        %Offspring(off, 1:start_index - 1) = Buff(1:start_index - 1);
     end
 % end function
 
